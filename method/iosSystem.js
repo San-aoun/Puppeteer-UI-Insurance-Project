03360dc4655f571381ba85acdce9782383c4e451
Piyathida Sanaoun (Numtarn) <39735390+San-aoun@users.noreply.github.com>
2019-07-14 11:21:05 +0700

    Update

---
+++ method/iosSystem.js
@@ -0,0 +1,22 @@
+module.exports = {
+    baseUrl: 'https://www.lazada.co.th/',
+    isHeadless: true,
+    slowMo: 0,
+    isDevtools: false,
+    lunchTimeOut: 0,
+    waitingTimeOut: 0,
+    viewportWidth: 800,
+    viewportHeight: 800,
+
+
+    /*browser:async function(){
+        puppeteer.launch({
+            headless:config.isHeadless,
+            slowMo:config.slowMo,
+            devtools:config.isDevtools,
+            timeout:config.lunchTimeOut})
+    }, 
+    page:async function(){
+
+    }*/
+}
