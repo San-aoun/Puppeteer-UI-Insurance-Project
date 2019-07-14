d32bca1b0f47283e14dfac5909d1400dc5ad1bfb
Piyathida Sanaoun (Numtarn) <39735390+San-aoun@users.noreply.github.com>
2019-07-14 11:30:06 +0700

    Update elem-leadonios.js

--- page-object/elem-leadonios.js
+++ page-object/elem-leadonios.js
@@ -1,5 +1,16 @@
 module.exports = {
-    actionHeader: '#topActionHeader',
-    catagoryElectronicMenu: '#Level_1_Category_No1',
-    subCatagoryElectronicMenu: '[data-spm="cate_1"] > li'
+    gotoIOS: 'li[title="ระบบประกันภัย"]',
+    dropdownLead: '#m_203',
+    checkCustomer: '#m_204',
+    clickAddCutomer: '#btnAdd',
+    addName: '#txtFirstName',
+    addTel: '#txtTel1',
+    clickSaveAsNewCS: '#btnSaveCustomer',
+    chooseJobs: 'img[src="/img/correct.png"]',
+    chooseTopic: '#ContentPlaceHolder1_ddlTopic',
+    chooseChanal: '#ContentPlaceHolder1_ddlChannel',
+    saveCreateLead: '#btnSave',
+    clickSendToIOS: '#btnFollowGroupRequestDetail',
+    getWorkIdNo: '[onclick^="openRequestDetailHistory"]',
+  
 }
